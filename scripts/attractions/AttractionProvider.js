dad6b3a0c1ba1a9be66f3fbb68915f03c29cf659
AdiGarmendia <dejima@gmail.com>
2019-12-17 09:29:38 -0600

    Added base fetch functions for parks, eatery and Bizzare

---
+++ scripts/attractions/AttractionProvider.js
@@ -0,0 +1,16 @@
+let Bizarro = []
+
+export const useBizzaro = () => {
+  return Bizarro.slice()
+}
+
+export const getBizzaro = () => {
+  return fetch("http://holidayroad.nss.team/bizarreries")
+      .then(response => response.json())
+      .then(
+          parsedBizzaro => {
+              console.table(parsedBizzaro)
+              Bizzaro = parsedBizzaro
+          }
+      )
+}
