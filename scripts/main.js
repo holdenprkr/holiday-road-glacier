b947aa3f963cd5d1f5b1e5439f9ab66fa51f5760
AdiGarmendia <dejima@gmail.com>
2019-12-19 10:43:22 -0600

    dialog box

--- scripts/main.js
+++ scripts/main.js
@@ -37,3 +37,4 @@ getBizzaro().then(
     attractionComponent()
   }
 )
+
--- scripts/parks/parksList.js
+++ scripts/parks/parksList.js
@@ -7,6 +7,18 @@ const contentElement = document.querySelector(".parkSection");
 export const parksList = () => {
   const parksCollection = useParks()
 
+  eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("pictures--")) {
+      // target a dialog element
+        const dialog = document.querySelector(".pictures")
+      // changes inner.html to display pictures
+        dialog.innerHTML = `${useParks(images)}`
+      // show dialog
+        dialog.showModal()
+    } 
+  })
+
+
   eventHub.addEventListener("parkSelected", customEvent => {
 
     for (let singlePark of parksCollection) {
@@ -20,3 +32,5 @@ export const parksList = () => {
       ${ParksComponent(park)}`
 
     }}
+
+
