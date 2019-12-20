e5c0dd4e645dda2829a03d48950f524bb74f00b5
AdiGarmendia <dejima@gmail.com>
2019-12-20 13:52:09 -0600

    url

--- scripts/parks/parks.js
+++ scripts/parks/parks.js
@@ -3,8 +3,8 @@ const ParksComponent = park => {
       <div class="park">
             <header class="park__header" id="h"><span style="font-weight: bold;">Name:</span> ${park.fullName}</header>
               <div><span style="font-weight: bold;">States:</span> ${park.states}</div>
-              <div><span style="font-weight: bold;">Website:</span> ${park.url}</div>
-              <div><span style="font-weight: bold;">Directions:</span> ${park.directionsUrl}</div>
+              <div><span style="font-weight: bold;">Website:</span><a href="${park.url}">National Parks Link</a></div>
+              <div><span style="font-weight: bold;">Directions:</span><a href="${park.directionsUrl}">Directions Link</a></div>
               <div><span style="font-weight: bold;">Description:</span> ${park.description}</div>
               <button id="pictures--${park.id}">Pictures</button>
               <dialog class="pictures">
