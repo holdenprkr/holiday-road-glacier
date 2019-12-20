60fb5527dabb3c5784d658e144b4839b714d6914
lcluischavez <lcluischavezz@gmail.com>
2019-12-20 00:03:48 -0600

    css

--- scripts/parks/parks.js
+++ scripts/parks/parks.js
@@ -1,7 +1,7 @@
 const ParksComponent = (park) => {
   return `
       <div class="park">
-            <header class="park__header"><span style="font-weight: bold;">Name:</span> ${park.fullName}</header>
+            <header class="park__header" id="h"><span style="font-weight: bold;">Name:</span> ${park.fullName}</header>
               <div><span style="font-weight: bold;">States:</span> ${park.states}</div>
               <div><span style="font-weight: bold;">Website:</span> ${park.url}</div>
               <div><span style="font-weight: bold;">Directions:</span> ${park.directionsUrl}</div>
